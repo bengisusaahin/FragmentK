11fe196b3223c4cbf7abab2404b363d3a20391d3
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-12-14 23:02:53 +0300

    Fragment Activity Connection

--- app/src/main/java/com/bengisusahin/fragmentk/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/fragmentk/MainActivity.kt
@@ -2,10 +2,28 @@ package com.bengisusahin.fragmentk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun firstFragment(view: View){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val firstFragment = FirstFragment()
+        fragmentTransaction.replace(R.id.frameLayout, firstFragment).commit()
+
+    }
+
+    fun secondFragment(view: View){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val secondFragment = SecondFragment()
+        fragmentTransaction.replace(R.id.frameLayout, secondFragment).commit()
     }
 }
